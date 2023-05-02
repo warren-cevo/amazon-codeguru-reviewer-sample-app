0de3d86634f94c09ca4344dc1b5d6ba7ca815efd
WarrenCevo <warren.abundo@cevo.com.au>
2023-05-02 16:11:59 +1000

    Add issue

--- src/main/java/com/shipmentEvents/util/Constants.java
+++ src/main/java/com/shipmentEvents/util/Constants.java
@@ -1,10 +1,15 @@
 package com.shipmentEvents.util;
 
+import com.sun.org.apache.bcel.internal.generic.PUSH;
+
 import java.util.Arrays;
 import java.util.List;
 
 public final class Constants {
 
     public static final List<String> BUCKETS_TO_PROCESS = Arrays.asList("shipment-events-from-scanner", "shipment-events-from-mobile-app");
-    public static final String SUMMARY_BUCKET = "shipment-events-summary"; 
+    public static final String SUMMARY_BUCKET = "shipment-events-summary";
+
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "P@ssw0rd";
 }
